0cf56a97bda5a53094e14b1e9c73114ca51553b6
Tomas D'Stefano <tomas_stefano@successoft.com>
2011-08-21 19:53:56 -0300

    Create an empty rails app for 09 episode

---
+++ 09_rake/episode_rake/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EpisodeRake::Application.config.secret_token = '5a0461fc46f3a8a14f078502e9d5dd4808eb48602b77f7ee1676781fcb84036483e0725842e635c577b13e313c96c8ea358f599559919c2734fd7e52bbe573a7'
